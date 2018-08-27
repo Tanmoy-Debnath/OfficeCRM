f00a9c6d5787b9a118986f2128e2c10f5344d2a6
Tanmoy-Debnath <tanmoy.bubt@gmail.com>
2018-08-27 11:55:58 +0600

    Adding office crm

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -304,7 +304,7 @@
             <footer class="footer">
                 <div class="container-fluid clearfix">
             <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
-              <a href="" target="_blank">Southeast Integrated Solutions Ltd.</a>. All rights reserved.</span>
+              <a href="" target="_blank">Southeast Integrated Solutions Ltd.</a>. All rights reserved.Tanmoy</span>
                     <!--<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
               <i class="mdi mdi-heart text-danger"></i>
             </span>
