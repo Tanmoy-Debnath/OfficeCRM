c6282438dc8395741cde90e4a905d305b2580fa8
mujahid15 <mujahidis2014@gmail.com>
2018-08-28 10:48:26 +0600

    nav link added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,9 @@
                 <a class="navbar-brand btn btn-success" href="{{ url('/login') }}">
                     {{ 'SOUTHEAST INTEGRATED SOLUTION' }}
                 </a>
+                <a class="navbar-brand btn btn-success" href="{{ url('/login') }}">
+                    {{ 'SOUTHEAST INTEGRATED SOLUTION' }}
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
