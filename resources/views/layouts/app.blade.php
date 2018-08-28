ffafb179f9366adaab06d9b598984e3bd0a8ea79
mujahid15 <mujahidis2014@gmail.com>
2018-08-28 10:56:39 +0600

    nav bar customized

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand btn btn-success" href="{{ url('/login') }}">
-                    {{ 'SOUTHEAST INTEGRATED SOLUTION' }}
-                </a>
-                <a class="navbar-brand btn btn-success" href="{{ url('/login') }}">
-                    {{ 'SOUTHEAST INTEGRATED SOLUTION' }}
+                    {{ 'Customer Relationship Management Software of SEIS' }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
